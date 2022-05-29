a262d5d6e8965349f28280a0d93ae15194305597
Adam Rackis <adamrackis@hotmail.com>
2022-05-29 12:16:20 -0500

    jsonRequest function

--- Sources/graphql-swift/network/networkRequest.swift
+++ Sources/graphql-swift/network/networkRequest.swift
@@ -4,36 +4,48 @@ enum NetworkRequestError: Error {
     case NoResponse
     case NonHttpResponse(URLResponse)
     case BadResponseCode(Int)
+    case BadData
+    case NonJsonResponse
 }
 
-func networkRequest(_ request: URLRequest) async throws -> Data {
+func jsonRequest(_ request: URLRequest) async throws -> [String: Any] {
+    let data = try await networkRequest(request)
+    
+    guard let data = data else {
+        throw NetworkRequestError.BadData
+    }
+    
+    guard let jsonObject = try? JSONSerialization.jsonObject(with: data) as? [String: Any] else {
+        print("Error: Cannot convert data to JSON object")
+        print("\n\n")
+        print(data)
+        
+        throw NetworkRequestError.NonJsonResponse
+    }
+    
+    return jsonObject
+}
+
+func networkRequest(_ request: URLRequest) async throws -> Data? {
     return try await withUnsafeThrowingContinuation { cont in
-        print("about to request")
         URLSession.shared.dataTask(with: request) { data, response, error in
-            print("Some response")
             if let error = error {
-                print("error", error)
                 cont.resume(throwing: error)
                 return
             }
             guard let response = response else {
-                print("A")
                 return cont.resume(throwing: NetworkRequestError.NoResponse)
             }
             guard let response = response as? HTTPURLResponse else {
-                print("B")
                 return cont.resume(throwing: NetworkRequestError.NonHttpResponse(response))
             }
             if !(200 ..< 299).contains(response.statusCode) {
                 return cont.resume(throwing: NetworkRequestError.BadResponseCode(response.statusCode))
             }
             
-            if let data = data {
-                print("Ayyyyy")
-                print(data)
-                cont.resume(returning: data)
-            }
+            return cont.resume(returning: data)
+            
         }.resume()
-        print("after resume")
     }
 }
+
