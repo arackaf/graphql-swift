a825c81a5999494a4864981c550e0bf7a844c25a
Adam Rackis <adamrackis@hotmail.com>
2022-05-30 22:19:49 -0500

    small tweak to type resolution code

--- Sources/graphql-swift/codegen/schemaTypes.swift
+++ Sources/graphql-swift/codegen/schemaTypes.swift
@@ -1,23 +1,23 @@
 import Foundation
 
+func getType(_ json: [String: Any]) -> String {
+    let kind: String? = json.get("kind")
+    if kind == "LIST" {
+        let nestedType = getType(json.val("ofType")!)
+        return "Array<\(nestedType)>"
+    } else {
+        return json.get("name") ?? getType(json.val("ofType")!)
+    }
+}
+
 public struct GraphqlFieldType: InitializableFromJSON {
     public let name: String
     public let type: String
     
-    static func getType(_ json: [String: Any]) -> String {
-        let kind: String? = json.get("kind")
-        if kind == "LIST" {
-            let nestedType = getType(json.val("ofType")!)
-            return "Array<\(nestedType)>"
-        } else {
-            return json.get("name") ?? getType(json.val("ofType")!)
-        }
-    }
-    
     public init(_ json: [String: Any]){
         name = json.get("name")!
         let typeField = json.val("type")!
-        type = Self.getType(typeField)
+        type = getType(typeField)
     }
 }
 
